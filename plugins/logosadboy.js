424c0a098f0868e6aec1e50383b7ab0df6e127b2
David <107624642+David0l1@users.noreply.github.com>
2022-07-05 15:14:09 +0700

    Create logosadboy.js

---
+++ plugins/logosadboy.js
@@ -0,0 +1,16 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn, args }) => {
+   response = args.join(' ').split('|')
+  if (!args[0]) throw 'Masukkan Parameter'
+  m.reply('Proses...')
+  let res = `https://rimurubotz-api.herokuapp.com/api/sadboy?text=${response[0]}`
+  conn.sendFile(m.chat, res, 'sadboy.png', `Sudah Jadi`, m, false)
+}
+handler.help = ['logololi'].map(v => v + ' <text>')
+handler.tags = ['nulis', 'anime']
+handler.command = /^(logololi)$/i
+handler.register = true
+
+handler.limit = true
+
+module.exports = handler
