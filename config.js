91df60ae9abee483a4935abb5489556c90386057
David0l1 <telkomselno1@gmail.com>
2022-08-25 11:44:40 +0700

    asw

---
+++ config.js
@@ -0,0 +1,147 @@
+// INI SC MASIH ADA BUG YA FIX IN AJA
+// BASE ILMANHDYT
+// SC ORI HYZER
+// CREATOR KRIZYN OFC
+// RECODE BY OSCAR OFC
+
+//=============『 Utama 』================== //
+global.owner = ['447578881250','6282128475388','6282160268332'] 
+global.mods = ['447578881250'] 
+global.prems = ['447578881250','6282128475388','6282160268332']
+
+//=============『 Info Owner 』============== //
+global.nameowner = 'ᴹᴿ Davitt'
+global.numberowner = '447578881250',
+global.instagram = 'https://instagram.com/davidpangrib001'
+global.github = 'https://github.com/David0l1'
+global.Youtube = 'https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ'
+global.sc = 'https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ'
+global.ss = 'https://youtu.be/3m7tRD4RklM'
+global.sss = 'https://youtu.be/PzUH1ApMCig'
+global.yt = 'https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ'
+global.dana = '-'
+global.pulsa = '082128475389'
+global.gopay = '-'
+
+//=============『 Info Bot 』=================//
+global.namebot = '❋۪۪ Onee BotZ'
+global.gc = 'https://chat.whatsapp.com/IqZke0cAG6G3iVsnB9myfL'
+global.web = 'https://api-oneebotz.herokuapp.com/' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
+global.webm = 'https://instagram.com/davidpangrib001'
+global.pulsa = '082128475388'
+global.nomer = 'https://wa.me/447578881250' //ganti jadi nomor mu
+global.price1 = 'Undefined Database'
+
+//=======『 Tampilan Dan Lainnya 』============//
+global.fotonya1 = 'https://telegra.ph/file/4dabaf764fa35a6b91f4e.jpg' //ganti jadi foto bot mu
+global.fotonya2 = 'https://telegra.ph/file/4dabaf764fa35a6b91f4e.jpg' //ini juga ganti
+global.fotonya3 = 'https://telegra.ph/file/4dabaf764fa35a6b91f4e.jpg' //ini juga ganti
+global.fotonya4 = 'https://telegra.ph/file/4dabaf764fa35a6b91f4e.jpg' //ini juga ganti
+global.fotonya5 = 'https://telegra.ph/file/4dabaf764fa35a6b91f4e.jpg' //ini juga ganti
+global.fotonya6 = 'https://telegra.ph/file/4dabaf764fa35a6b91f4e.jpg' //ini ganti pakai foto sosialmedia mu
+global.fotonya7 = 'https://telegra.ph/file/4dabaf764fa35a6b91f4e.jpg'
+global.fotonya8 = 'https://telegra.ph/file/4dabaf764fa35a6b91f4e.jpg' //foto session
+global.donasi = 'https://telegra.ph/file/4dabaf764fa35a6b91f4e.jpg' //ganti pakai poto payment atau apa
+global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
+global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
+global.wm = '❋۪۪ Onee - BotZ_MD'
+global.watermark = wm
+global.wm2 = '❋۪۪ Onee - BotZ_MD'
+global.wm3 = 'ᯤ OneeBotZ'
+global.htki = '––––––『' 
+global.htka = '』––––––'
+global.media = 'https://telegra.ph/file/72168c6f1aaa90315c369.jpg'
+global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.wait = '*WAIT!*\n_Mohon Tunggu Sebentar . . ._
+global.eror = '*ERROR!*\n_Terjadi Kesalahan_'
+global.benar = 'Benar ✅\n'
+global.salah = 'Salah ❌\n'
+global.stiker_wait = 'Jangan Spam Botnya :D'
+global.packname = '『 One Botz 』\n\n\n\n\n\n\n\n\n\n'
+global.author = ''
+
+//=============『 Apikey 』================== //
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  amel: 'https://melcanz.com',
+    bx: 'https://bx-hunter.herokuapp.com',
+  dhnjing: 'https://dhnjing.xyz',
+  hardianto: 'https://hardianto-chan.herokuapp.com',
+  jonaz: 'https://jonaz-api-v2.herokuapp.com',
+  neoxr: 'https://neoxr-api.herokuapp.com',
+  nrtm: 'https://nurutomo.herokuapp.com',
+  xteam: 'https://api.xteam.xyz',
+  nzcha: 'http://nzcha-apii.herokuapp.com',
+  bg: 'http://bochil.ddns.net',
+  fdci: 'https://api.fdci.se',
+  dzx: 'https://api.dhamzxploit.my.id',
+  bsbt: 'https://bsbt-api-rest.herokuapp.com',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  zeks: 'https://api.zeks.xyz',
+  zekais: 'http://zekais-api.herokuapp.com',
+  hardianto: 'https://hardianto-chan.herokuapp.com',
+  pencarikode: 'https://pencarikode.xyz', 
+  erdwepe: 'https://erdwpe-api.herokuapp.com',
+  lolhuman: 'https://api.lolhuman.xyz',
+  LeysCoder: 'https://leyscoders-api.herokuapp.com',
+  rey: 'https://server-api-rey.herokuapp.com'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://melcanz.com': 'elaina',
+  'https://server-api-rey.herokuapp.com': 'apirey',
+  'https://api.xteam.xyz': 'd37372311698ed1d',
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://bsbt-api-rest.herokuapp.com': 'benniismael',
+  'https://api.zeks.xyz': 'apivinz',
+  'https://hardianto-chan.herokuapp.com': 'hardianto',
+  'https://pencarikode.xyz': 'pais', 
+  'https://leyscoders-api.herokuapp.com': 'dappakntlll',
+  'https://zekais-api.herokuapp.com': 'zekais',
+  'https://api.lolhuman.xyz': '7047f08f5a0ea4bcdf55cd72',
+}
+
+//=============『 RPG GAMES 』================== //
+global.multiplier = 69 // The higher, The harder levelup
+global.rpg = {
+  emoticon(string) {
+    string = string.toLowerCase()
+    let emot = {
+      exp: '✉️',
+      money: '💵',
+      potion: '🥤',
+      diamond: '💎',
+      common: '📦',
+      uncommon: '🎁',
+      mythic: '🗳️',
+      legendary: '🗃️',
+      pet: '🎁',
+      sampah: '🗑',
+      armor: '🥼',
+      sword: '⚔️',
+      kayu: '🪵',
+      batu: '🪨',
+      string: '🕸️',
+      kuda: '🐎',
+      kucing: '🐈' ,
+      anjing: '🐕',
+      petFood: '🍖',
+      gold: '👑',
+      emerald: '💚'
+    }
+    let results = Object.keys(emot).map(v => [v, new RegExp(v, 'gi')]).filter(v => v[1].test(string))
+    if (!results.length) return ''
+    else return emot[results[0][0]]
+  }
+}
+
+//===========『 Jangan Di Ubah 』================ //
+let fs = require('fs')
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
