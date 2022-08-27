f7d5f352b2e0ff2dffe24bbe9af36cb3f29121b5
David0l1 <telkomselno1@gmail.com>
2022-08-27 12:27:44 +0700

    Ahh

--- plugins/anime-neko.js
+++ plugins/anime-neko.js
@@ -1,15 +1,15 @@
-import fetch from 'node-fetch'
+// xie hua piao piao batman membuka kulkas dan mngambil semangka lalu memberikan mayo di atas semangka
 
-let handler = async (m, { conn }) => {
-	let ne = await (await fetch('https://github.com/David0l1/Ntahh/raw/MellisA/src/neko-scraper.txt')).text()
-    let nek = ne.split('\n')
-    let neko = pickRandom(nek)
-	conn.sendButton(m.chat, 'Nyaww~ 🐾💗', wm, neko, [['Next','.neko']],m)
+let fetch = require('node-fetch')
+let handler = async(m, { conn }) => {
+  let res = await fetch('https://api.waifu.pics/sfw/neko')
+  if (!res.ok) throw await res.text()
+  let json = await res.json()
+  if (!json.url) throw 'Error!'
+  conn.sendFile(m.chat, json.url, '', 'Nyaa', m)
 }
-handler.command = /^(neko)$/i
-handler.tags = ['anime']
 handler.help = ['neko']
-export default handler
-function pickRandom(list) {
-  return list[Math.floor(Math.random() * list.length)]
-}
+handler.tags = ['anime']
+handler.command = /^neko$/i
+
+module.exports = handler
