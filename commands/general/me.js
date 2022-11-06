fcf0b9da39834938bb4190633fc23c29b089c86f
davidpangrib001 <davidpangrib001@gmail.com>
2022-11-07 00:01:14 +0700

    ,,,

---
+++ commands/general/me.js
@@ -0,0 +1,21 @@
+const { ICommand } = require('@libs/builders/command')
+const users = require('@database/services/users')
+
+/**
+ * @type { ICommand }
+ */
+module.exports = {
+    category: 'about',
+    description: 'Show your stats.',
+    callback: async ({ msg }) => {
+        const user = await users.findOne(msg.senderNumber)
+        return msg.reply(`
+User Number : ${user.user_jid}
+User Limit : ${user.user_limit}
+User Level : Lv. ${user.user_level}
+User Exp : ${user.user_exp} XP
+User Premium : ${user.user_premium ? 'Yes' : 'No'}
+User Registered At : ${user.user_create_at}
+`)
+    },
+}
---
+++ commands/misc/ping.js
@@ -0,0 +1,15 @@
+const { ICommand } = require('@libs/builders/command')
+const moment = require('moment-timezone')
+
+/**
+ * @type { ICommand }
+ */
+module.exports = {
+    aliases: ['p'],
+    category: 'misc',
+    description: 'Test bot response.',
+    cooldown: 10 * 1000,
+    callback: async ({ msg, message }) => {
+        return msg.reply(`*_${moment.duration(Date.now() - parseInt(message.messageTimestamp.toString()) * 1000).asSeconds()} second(s)_*`)
+    },
+}
